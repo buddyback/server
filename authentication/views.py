2d76d3f87b47d276b72e3f4aa165f877d91fba13
Marco Realacci <marco@marcorealacci.me>
2025-04-24 21:17:48 +0200

    Revert "format code"
    This reverts commit ca881857f09815676e401c1631df0c31d8497c26.

--- authentication/views.py
+++ authentication/views.py
@@ -4,7 +4,11 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 
 class CustomProviderAuthView(ProviderAuthView):
@@ -12,26 +16,26 @@ class CustomProviderAuthView(ProviderAuthView):
         response = super().post(request, *args, **kwargs)
 
         if response.status_code == 201:
-            access_token = response.data.get("access")
-            refresh_token = response.data.get("refresh")
+            access_token = response.data.get('access')
+            refresh_token = response.data.get('refresh')
 
             response.set_cookie(
-                "access",
+                'access',
                 access_token,
                 max_age=settings.AUTH_ACCESS_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
-                samesite=settings.AUTH_COOKIE_SAMESITE,
+                samesite=settings.AUTH_COOKIE_SAMESITE
             )
             response.set_cookie(
-                "refresh",
+                'refresh',
                 refresh_token,
                 max_age=settings.AUTH_REFRESH_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
-                samesite=settings.AUTH_COOKIE_SAMESITE,
+                samesite=settings.AUTH_COOKIE_SAMESITE
             )
 
         return response
@@ -42,31 +46,31 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         response = super().post(request, *args, **kwargs)
 
         if response.status_code == 200:
-            username = request.data.get("username")
+            username = request.data.get('username')
 
-            access_token = response.data.get("access")
-            refresh_token = response.data.get("refresh")
+            access_token = response.data.get('access')
+            refresh_token = response.data.get('refresh')
 
             response.set_cookie(
-                "access",
+                'access',
                 access_token,
                 max_age=settings.AUTH_ACCESS_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
-                samesite=settings.AUTH_COOKIE_SAMESITE,
+                samesite=settings.AUTH_COOKIE_SAMESITE
             )
             response.set_cookie(
-                "refresh",
+                'refresh',
                 refresh_token,
                 max_age=settings.AUTH_REFRESH_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
-                samesite=settings.AUTH_COOKIE_SAMESITE,
+                samesite=settings.AUTH_COOKIE_SAMESITE
             )
 
-            response.data["username"] = username
+            response.data['username'] = username
 
         return response
 
@@ -74,24 +78,24 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 class CustomTokenRefreshView(TokenRefreshView):
     def post(self, request, *args, **kwargs):
 
-        refresh_token = request.COOKIES.get("refresh")
+        refresh_token = request.COOKIES.get('refresh')
 
         if refresh_token:
-            request.data["refresh"] = refresh_token
+            request.data['refresh'] = refresh_token
 
         response = super().post(request, *args, **kwargs)
 
         if response.status_code == 200:
-            access_token = response.data.get("access")
+            access_token = response.data.get('access')
 
             response.set_cookie(
-                "access",
+                'access',
                 access_token,
                 max_age=settings.AUTH_ACCESS_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
-                samesite=settings.AUTH_COOKIE_SAMESITE,
+                samesite=settings.AUTH_COOKIE_SAMESITE
             )
 
         return response
@@ -99,10 +103,10 @@ class CustomTokenRefreshView(TokenRefreshView):
 
 class CustomTokenVerifyView(TokenVerifyView):
     def post(self, request, *args, **kwargs):
-        access_token = request.COOKIES.get("access")
+        access_token = request.COOKIES.get('access')
 
         if access_token:
-            request.data["token"] = access_token
+            request.data['token'] = access_token
 
         return super().post(request, *args, **kwargs)
 
@@ -112,8 +116,8 @@ class LogoutView(APIView):
 
     def post(self, request, *args, **kwargs):
         response = Response(status=status.HTTP_204_NO_CONTENT)
-        response.delete_cookie("access")
-        response.delete_cookie("refresh")
+        response.delete_cookie('access')
+        response.delete_cookie('refresh')
 
         return response
 
@@ -124,8 +128,8 @@ class GetRequestUser(APIView):
     def get(self, request):
         return Response(
             {
-                "username": request.user.username,
-                "is_staff": request.user.is_staff,
-                "is_moderator": request.user.profile.is_moderator,
+                'username': request.user.username,
+                'is_staff': request.user.is_staff,
+                'is_moderator': request.user.profile.is_moderator
             }
         )
