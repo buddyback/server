fdac047d4f026877adbcedb19b81b0dd2700b4ff
federico <fraponi98@gmail.com>
2025-04-29 17:47:42 +0200

    fix path

--- server/settings.py
+++ server/settings.py
@@ -186,7 +186,7 @@ WSGI_APPLICATION = "server.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": getenv("DB_PATH", BASE_DIR) / "db.sqlite3",
+        "NAME": str(getenv("DB_PATH", BASE_DIR / "db.sqlite3")),
     }
 }
 
